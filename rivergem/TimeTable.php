b776fe699f41528df02c670fbd64fbace43ed73d
AndrewWillamsq <rivergem061@gmail.com>
2026-06-17 15:09:40 +0930

    k

---
+++ rivergem/TimeTable.php
@@ -0,0 +1,13 @@
+<?php include("./header.php");
+?>
+$days = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday,' 'Friday']; 
+
+$time-slots = [
+'9:00-10:00',
+'10:00-11:00', 
+'11:00-12:00', 
+'12:00-1:00',
+1:00-2:00', 
+2:00-3:20',  
+
+]; 
