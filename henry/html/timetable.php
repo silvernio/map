36631102b0b27ccefabb4c7fc5d6fbd40a3cb93b
BlazingFish <115965185+BlazingFish@users.noreply.github.com>
2026-06-24 09:25:46 +0930

    changed from using an html file to a php file

--- henry/html/timetable.php
+++
@@ -1,5 +0,0 @@
-<?php include "../header.php";?>
-"hello hello hello"
-
-</body>
-</html>
---
+++ henry/index.php
@@ -0,0 +1,24 @@
+<?php include "../header.php";?>
+"hello hello hello"
+
+<head>
+</head>
+
+<body>
+    <style id="style"></style>
+    <div class="tableWrapper" style="border: 1px solid black;">
+        <table id="timetable">
+            <tr>
+                <th>
+                    test
+                </th>
+            </tr>
+        </table>
+    </div>
+
+    <!-- <input type = "color"></input> -->
+
+    <script src="js/timetable.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</body>
+</html>
