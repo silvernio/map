49bdd631c22c4ef9e00d99543cbafc1573f45bf7
SilverSpace505 <112796153+SilverSpace505@users.noreply.github.com>
2026-06-24 09:33:47 +0930

    start editor

--- silver/index.php
+++ silver/index.php
@@ -1,11 +1,10 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>editor</title>
-</head>
-<body>
-    <h1>autodeploy may be working</h1>
-    <script type="module" src="./script.js"></script>
+<?php include "../header.php" ?>
+
+<h1>Editor</h1>
+
+<div id="sidebar"></div>
+
+<canvas id="canvas"></canvas>
+
 </body>
 </html>
