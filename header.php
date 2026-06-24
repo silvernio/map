e18ab3e2a79459e97e214924e58ecd473c90144e
SilverSpace505 <112796153+SilverSpace505@users.noreply.github.com>
2026-06-24 09:42:38 +0930

    fix header styling?

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <script src="
 https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.umd.min.js
 "></script>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
 <body>
     <!-- nav -->
 <nav class="navbar" role="navigation" aria-label="main navigation">
