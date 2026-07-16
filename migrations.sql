82a9237b23816c2acf2b1c441d3ab3f8bf52005b
SilverSpace505 <112796153+SilverSpace505@users.noreply.github.com>
2026-07-16 17:09:57 +0930

    rename classrooms table

--- migrations.sql
+++ migrations.sql
@@ -3,3 +3,7 @@
 -- 25/06/26 - Sam - Updated column names for lessons and classrooms
 ALTER TABLE `classrooms` CHANGE `name` `classroom_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
 ALTER TABLE `lessons` CHANGE `name` `lesson_name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+-- 16/07/26 - Silver - Rename classrooms table to rooms
+RENAME TABLE `map`.`classrooms` TO `map`.`rooms`;
+ALTER TABLE `rooms` CHANGE `classroom_id` `room_id` INT(11) NOT NULL AUTO_INCREMENT;
